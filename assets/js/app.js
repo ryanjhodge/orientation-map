5455f0d229699b88f90d1e43041ef170e3a28de3
Ryan J. Hodge <ryanjhodge@users.noreply.github.com>
2017-01-26 11:11:27 -0800

    Initialize application skeleton

---
+++ assets/js/app.js
@@ -0,0 +1,74 @@
+/* globals $, L */
+
+var App = {
+  initialize: function () {},
+  localities: {
+    soleduck: {
+      coordinates: [47.98, -123.88],
+      zoom: 11
+    },
+    lakeCrescent: {
+      coordinates: [48.05, -123.82],
+      zoom: 13
+    },
+    elwha: {
+      coordinates: [47.98, -123.59],
+      zoom: 13
+    }
+  }
+};
+
+var NPMap = {
+  baseLayers: [
+    'nps-parkTiles3',
+    'nps-parkTiles3Imagery',
+    'nps-parkTiles3Slate'
+  ],
+  center: {
+    lat: 47.5830,
+    lng: -123.4561
+  },
+  description: 'Orientation Map',
+  div: 'map',
+  fullscreenControl: true,
+  hooks: {
+    preinit: function (callback) {
+      L.npmap.util._.appendJsFile([
+        'https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js'
+      ], function () {
+        L.npmap.util._.appendJsFile([
+          'https://www.nps.gov/lib/bootstrap/3.3.2/js/nps-bootstrap.min.js'
+        ], function () {
+          L.npmap.util._.appendCssFile([
+            'https://www.nps.gov/lib/npmap.js/3.0.18/npmap.min.css'
+          ]);
+        });
+      });
+      callback();
+    },
+    init: function (callback) {
+      App.initialize();
+      callback();
+    }
+  },
+  // maxBounds: [
+  //   [45.310, -127.225],
+  //   [49.499, -120.552]
+  // ],
+  minZoom: 8,
+  overlays: [{
+    preset: 'nps-places-pois',
+    unitCodes: [
+      'olym'
+    ]
+  }],
+  title: 'Olympic National Park',
+  zoom: 8,
+  zoomdisplayControl: true
+};
+
+(function () {
+  var s = document.createElement('script');
+  s.src = 'https://www.nps.gov/lib/npmap.js/3.0.18/npmap-bootstrap.min.js';
+  document.body.appendChild(s);
+})();
